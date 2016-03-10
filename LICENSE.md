ed9ce0fe7028c4080b96fb5ec7a4f4bfe8bbc26d
Jerry Hampton <hamptonjt@hotmail.com>
2016-03-10 20:10:16 +0000

    Removing Clementine boilerplate from server.js, readme.md, package.json, Licence.md and adding code for FCC - Timestamp

--- LICENSE.md
+++ LICENSE.md
@@ -1,5 +1,5 @@
-Clementine.js
-Copyright (c) 2015 Blake Johnston. All rights reserved.
+FCC - Timestamp Microservice
+Copyright (c) 2016 Jerry Hampton. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- server.js
+++ server.js
@@ -1,31 +1,84 @@
 'use strict';
 
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+//var routes = require('./app/routes/index.js');
+//var mongoose = require('mongoose');
+//var passport = require('passport');
+//var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+//require('dotenv').load();
+//require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+//mongoose.connect(process.env.MONGO_URI);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+//app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+//app.use('/public', express.static(process.cwd() + '/public'));
+//app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+//app.use(session({
+	//secret: 'secretClementine',
+	//resave: false,
+	//saveUninitialized: true
+//}));
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
-routes(app, passport);
+//routes(app, passport);
+app.get('/', function(req,res) {
+	res.end('Add a string to the end of the URL to use the microservice...\n\nFor example: \n\nhttps://timestamp-hamptonjt.c9users.io/April%2011,%201977')
+	
+})
+
+app.get('/:date', function(req, res) {
+	var timestamp = {
+		"unix": null,
+		"natural": null
+	}
+	
+	var inputDate = req.params.date
+	var inputDateAsDate = new Date()
+	var d = Date.parse(inputDate)
+	var naturalDate
+	
+	inputDateAsDate.setTime(inputDate * 1000) // convert to milliseconds...
+	
+	res.writeHead(200, {'Accept': 'application/json'})
+	
+	if ( isNaN(d) ) {  // d.valueOf() could also work
+		// date is not valid as a natural date - check to see if it was a unix date..
+		d = inputDateAsDate
+		var unixDate = Date.parse(d)
+		if ( isNaN(unixDate) ) {
+			// date is invalid
+			timestamp.unix = null
+			timestamp.natural = null
+			console.log('Invalid Date by time...')
+			res.end(JSON.stringify(timestamp))
+		}
+		else {
+			// date is valid...
+			naturalDate = new Date(unixDate)
+			timestamp.unix = inputDate 
+			timestamp.natural = months[naturalDate.getMonth()] + ' ' + naturalDate.getDate() + ', ' + naturalDate.getFullYear()
+			res.end(JSON.stringify(timestamp))
+		}
+	}
+	else {
+		// date is valid
+		naturalDate = new Date(d)
+		timestamp.unix = d / 1000
+		timestamp.natural = months[naturalDate.getMonth()] + ' ' + naturalDate.getDate() + ', ' + naturalDate.getFullYear()
+		res.end(JSON.stringify(timestamp))
+	}
+	
+	console.log('Input Date:', inputDate)
+	console.log('Unix:', timestamp.unix)
+	console.log('Natural:', timestamp.natural)
+})
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
