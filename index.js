0164a719eb7d770cb1b8fe487b46f5722fe9df5b
Jerry Hampton <hamptonjt@hotmail.com>
2020-05-21 08:16:41 -0400

    fixing unix date conversion

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
 var express = require('express');
 var app = express();
 
@@ -11,44 +9,41 @@ app.get('/', function(req,res) {
 
 app.get('/:date', function(req, res) {
 	var timestamp = {
-		"unix": null,
-		"utc": null
+		"unix": '',
+		"utc": ''
 	}
 	
 	var inputDate = req.params.date
-	var inputDateAsDate = new Date(inputDate)
 	var d = Date.parse(inputDate)
 	
-	res.writeHead(200, {'Accept': 'application/json'})
 	
 	if ( isNaN(d) ) {  
 		// date is not valid as a natural date - check to see if it was a unix date..
-		// d = inputDateAsDate
-		var unixDate = Date.parse(inputDateAsDate)
-		if ( isNaN(unixDate) ) {
+		if ( isNaN(inputDate) ) {
 			// date is invalid
-			timestamp.unix = null
-			timestamp.utc= null
+			timestamp.unix = ''
+			timestamp.utc= ''
 			console.log('Invalid Date by time...')
-			res.end(JSON.stringify({"error": "Invalid Date"}))
+			res.send({"error": "Invalid Date"})
 		}
 		else {
 			// date is valid...
-			timestamp.unix = inputDate 
-			timestamp.utc = new Date(unixDate).toUTCString()
-			res.end(JSON.stringify(timestamp))
+			timestamp.unix = inputDate
+			timestamp.utc = new Date(inputDate * 1).toUTCString()
+			res.send(timestamp)
 		}
 	}
 	else {
 		// date is valid
-		timestamp.unix = d / 1000
+		timestamp.unix = d
 		timestamp.utc = new Date(d).toUTCString()
-		res.end(JSON.stringify(timestamp))
+		res.send(timestamp)
 	}
 	
 	console.log('Input:', inputDate)
 	console.log('Unix:', timestamp.unix)
 	console.log('UTC:', timestamp.utc)
+	res.end()
 })
 
 var port = process.env.PORT || 8080;
